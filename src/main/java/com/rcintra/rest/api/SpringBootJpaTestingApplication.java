db03a6069508f8cc3dbaff825732b5aa4a9b3f0d
Rafael Cintra <ra.cintra@gmail.com>
2021-07-07 17:39:28 -0300

    Add Starter Web and Logger

--- src/main/java/com/rcintra/rest/api/SpringBootJpaTestingApplication.java
+++ src/main/java/com/rcintra/rest/api/SpringBootJpaTestingApplication.java
@@ -1,12 +1,24 @@
 package com.rcintra.rest.api;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootJpaTestingApplication {
+	
+	public static final Logger logger = LoggerFactory.getLogger(SpringBootJpaTestingApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application started...");
+	}
 
 	public static void main(String[] args) {
+		logger.info("Application executed...");
 		SpringApplication.run(SpringBootJpaTestingApplication.class, args);
 	}
 
--- src/test/java/com/rcintra/rest/api/SpringBootJpaTestingApplicationTests.java
+++ src/test/java/com/rcintra/rest/api/SpringBootJpaTestingApplicationTests.java
@@ -1,13 +1,21 @@
 package com.rcintra.rest.api;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootJpaTestingApplicationTests {
+	
+	public static final Logger logger = LoggerFactory.getLogger(SpringBootJpaTestingApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		logger.info("Test case executing...");
+		assertEquals(true, true);
 	}
 
 }
